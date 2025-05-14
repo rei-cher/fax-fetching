963516e8807e387272e7f59455aa5e56262d2b01
Pavel <p4shage@gmail.com>
2025-05-14 19:26:48 -0400

    denial filtes
    json result that has data iterating over the files in folders

--- debug/text_box.py
+++ debug/text_box.py
@@ -1,21 +1,10 @@
-import cv2, pytesseract, re, os
+import cv2, pytesseract, re, os, json, time
 from pdf2image import convert_from_path
 
-# name = "Approval_ANTONIA-FRENCH_n-a_OPZELURA"  # Healthfirst approval
-# name = "Approval_Angelin-Samuel_01-20-2014_ADAPALENE"   # Wellpoint approval
-# name = "Approval_Bryan-Bhagwandat _09-04-2006_CLOBETASOL"   # Fideli care approval
-# name = "Approval_CRISTIA-HERNANDEZ_3-12-2010_Doxycycline"   # Horizon approval
-# name = "Approval_NALINI-AJODHA_2-11-1969_TACROLIMUS"    # Prime Therapeutics - private horizon
-# name = "Approval_RAMONA-ROSA-ROSARIO_1-27-1945_TACROLIMUS"  # TODO: currently doesnt work ----- FIX: working
-# name = "Approval_Danny-Virovlyansky_n-a_ISOTRETINOIN"
-# pdf = f"C:\\Users\\OFFICE\\Documents\\dump\\05-09-2025\\approvals\\{name}.pdf"
-# name = "Approval_Trang-Kieu_03-24-1979_Dupixent"
-# name = "Approval_Unknown_Unknown_Unknown_417724dd-b096-45d2-8044-8d80049ee792"
-# pdf = f"C:\\Users\\OFFICE\\Documents\\dump\\05-13-2025\\approvals\\{name}.pdf"
-pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\OFFICE\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+path = "C:\\Users\\OFFICE\\Documents\\dump\\05-13-2025"
 
-# since the pdf is not an image, we have to convert each page into the image
-pages = convert_from_path(pdf, poppler_path="C:\\Users\\OFFICE\\Documents\\poppler-24.08.0\\Library\\bin", dpi=300) 
+pytesseract.pytesseract.tesseract_cmd = "C:\\Users\\OFFICE\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+        
 
 insurance_types = [
     "Healthfirst",
@@ -26,7 +15,9 @@ insurance_types = [
     "Clover Health",
     "UnitedHealthcare",    # UnitedHealthCare
     "Express Scripts",
-    "WellCare"
+    "WellCare",
+    "Aetna",
+    "Horizon"
 ]
 
 approval_patterns = [
@@ -45,10 +36,12 @@ approval_patterns = [
 
 # TODO: adjust denial letter patters 
 denial_patterns = [
-    r"\bReason for Denial:\b",
-    r"\b__X__ Denying you request\b",
-    r"\bRE: Denial of request for coverage\b",
-    r"\bRE: Denial of request for coverage\b",
+    r"Reason for Denial:",
+    r"__X__ Denying your request for",
+    r"__X__Denying your request for",
+    r"RE: Denial of request for coverage",
+    r"denied the request for the following reason",
+    r"We are unable to approve your request for this drug",
 ]
 
 def determine_letter_type(text):
@@ -88,7 +81,7 @@ def find_approval_entities(text, insurance):
                 patient_drug = patient_drug_match.group(1).split(" ")[0]
 
         case "Wellpoint":
-            patient_info_match = re.search(r'Member:\s*\d+,\s*([A-Za-z\s]+),\s*(\d{2}/\d{2}/\d{4})', text)
+            patient_info_match = re.search(r'Member:\s*\d+,\s*([A-Za-z\s]+),\s*(\d{1,2}/\d{1,2}/\d{4})', text)
             patient_drug_match = re.search(r'follows:\s*\n*\s*([A-Z][A-Z0-9]+)', text)
             if patient_info_match:
                 patient_name = patient_info_match.group(1).strip().replace(" ", "-")
@@ -98,7 +91,7 @@ def find_approval_entities(text, insurance):
 
         case "Fidelis Care":
             patient_name_match = re.search(r'Member:\s*([A-Za-z\s\-]+)\s+ID', text) or re.search(r'Member Name:\s*([A-Za-z\s\-]+)\s+Member', text)
-            patient_dob_match = re.search(r'DOB:\s*(\d{2}/\d{2}/\d{4})', text)
+            patient_dob_match = re.search(r'DOB:\s*(\d{1,2}/\d{1,2}/\d{4})', text)
             patient_drug_match = re.search(r'Question:\s*\n*\s*([A-Z][A-Z0-9]+)', text) or re.search(r'Service:\s*\n*\s*([A-Z][A-Z0-9]+)', text)
             if patient_name_match:
                 patient_name = patient_name_match.group(1).strip().replace(" ", "-")
@@ -108,7 +101,7 @@ def find_approval_entities(text, insurance):
                 patient_drug = patient_drug_match.group(1)
         
         case "HORIZON":
-            pattern = re.search(r'has requested\s+([A-Za-z\s]+?)\s+for\s+([A-Z\s]+?),\s*ID\s*#\s*\d+,\s*DOB\s*([0-9/]+)', text, re.DOTALL)
+            pattern = re.search(r'has requested\s+([A-Za-z\s]+?)\s+for\s+([A-Z\s]+?),\s*ID\s*#\s*\d+,\s*DOB\s*(\d{1,2}/\d{1,2}/\d{4})', text, re.DOTALL)
             if pattern:
                 patient_name = pattern.group(2).replace('\n', ' ').strip().replace(" ", "-")
                 patient_dob = pattern.group(3).strip().replace("/", "-")
@@ -151,7 +144,7 @@ def find_approval_entities(text, insurance):
         
         case "UnitedHealthcare":
             patient_name_match = re.search(r'Member Name_ \| ([A-Za-z]+(?: [A-Za-z]+)+)', text) or re.search(r'Patient:\s*([A-Za-z\s\-]+)\s+Case', text)
-            patient_dob_match = re.search(r'Member DOB (\d{2}/\d{2}/\d{4})', text)
+            patient_dob_match = re.search(r'Member DOB (\d{1,2}/\d{1,2}/\d{4})', text)
             patient_drug_match = re.search(r'Drug Name (.+?) Approval', text) or re.search(r'inform you that the (.+?) requested', text)
             if patient_name_match:
                 patient_name = patient_name_match.group(1).strip().replace(" ", "-")
@@ -162,7 +155,7 @@ def find_approval_entities(text, insurance):
 
         case "Express Scripts":
             patient_name_match = re.search(r'Patient:\s*([A-Za-z\s\-]+)\s+Physician', text) or re.search(r'Memver name:\s*([A-Za-z\s\-]+)\s+Member', text)
-            patient_dob_match = re.search(r'Patient DOB: (\d{2}/\d{2}/\d{4})', text)
+            patient_dob_match = re.search(r'Patient DOB: (\d{1,2}/\d{1,2}/\d{4})', text)
             patient_drug_match = re.search(r'request for your patient to obtain coverage for (.+?) under', text)
             if patient_name_match:
                 patient_name = patient_name_match.group(1).strip().replace(" ", "-")
@@ -180,6 +173,53 @@ def find_approval_entities(text, insurance):
                 patient_drug = patient_drug_match.group(1).split(" ")[0]
 
     return patient_name, patient_dob, patient_drug
+
+def find_denial_entitied(text, insurance):
+    """
+    Based on the extracted ocr text and insurnace type from determine_insurance, get the patient info.
+    For each insurance there is its own regex for patient info.
+    """
+
+    patient_name = None
+    patient_dob = None
+    patient_drug = None
+
+    match insurance:
+        case "Fidelis Care":
+            patient_name_match = re.search(r'Member:\s*([A-Za-z\s\-]+)\s+1D', text) or re.search(r'Member:\s*([A-Za-z\s\-]+)\s+ID', text)
+            patient_dob_match = re.search(r'DOB:\s*(\d{1,2}/\d{1,2}/\d{4})', text)
+            patient_drug_match = re.search(r'Question:\s*\n*\s*([A-Z][A-Z0-9]+)', text) or re.search(r'Service:\s*\n*\s*([A-Z][A-Z0-9]+)', text)
+            if patient_name_match:
+                patient_name = patient_name_match.group(1).strip().replace(" ", "-")
+            if patient_dob_match:
+                patient_dob = patient_dob_match.group(1).replace("/", "-")
+            if patient_drug_match:
+                patient_drug = patient_drug_match.group(1)
+
+        case "Aetna":
+            patient_name_match = re.search(r'Re:\s*([A-Za-z\s\-]+)\s+DOB', text)
+            patient_dob_match = re.search(r'DOB:\s*(\d{1,2}/\d{1,2}/\d{4})', text)
+            patient_drug_match = re.search(r'request for coverage of (.+?) for you', text)
+            if patient_name_match:
+                patient_name = patient_name_match.group(1).strip().replace(" ", "-")
+            if patient_dob_match:
+                patient_dob = patient_dob_match.group(1).replace("/", "-")
+            if patient_drug_match:
+                patient_drug = patient_drug_match.group(1).split(" ")[0]
+
+        case "Horizon":
+            patient_name_match = re.search(r'Dear\s+([A-Z\s\-]+):', text)
+            patient_dob_match = re.search(r'Date of Birth:\s*(\d{1,2}/\d{1,2}/\d{4})', text)
+            patient_drug_match = re.search(r'request for (.+?) services', text)
+            if patient_name_match:
+                patient_name = patient_name_match.group(1).strip().replace(" ", "-")
+            if patient_dob_match:
+                patient_dob = patient_dob_match.group(1).replace("/", "-")
+            if patient_drug_match:
+                patient_drug = patient_drug_match.group(1).split(" ")[0]
+
+    return patient_name, patient_dob, patient_drug
+
 
 def merge_boxes(boxes, overlap_thresh=30):
     merged = []
@@ -214,86 +254,121 @@ def merge_boxes(boxes, overlap_thresh=30):
         ))
     return merged
 
-result = []
 
-# iterate over tha pages sava the page as image
-for i, page in enumerate(pages):
-    page.save(f'./debug/images/page_{i}.jpg', 'JPEG')
+all_results = []
+count = 0
+start_time = time.perf_counter()
+for root, dirs, files in os.walk(path):
+    for file in files:
+        if not file.lower().endswith(".pdf"):
+            continue
 
-    # read an image with cv2
-    image = cv2.imread(f'./debug/images/page_{i}.jpg')
+        file_path = os.path.join(root, file)
+        print(file_path)
+        # since the pdf is not an image, we have to convert each page into the image
+        pages = convert_from_path(file_path, poppler_path="C:\\Users\\OFFICE\\Documents\\poppler-24.08.0\\Library\\bin", dpi=300) 
 
-    # gray the image
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        result = []
 
-    # blur an image
-    blur = cv2.GaussianBlur(gray, (7,7), 0)
+        # iterate over tha pages sava the page as image
+        for i, page in enumerate(pages):
+            page.save(f'./debug/images/page_{i}.jpg', 'JPEG')
 
-    # threshold image
-    thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
-    adaptive_tresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 51, 5)
-    # cv2.imshow("thresh", cv2.resize(thresh, (560, 900)))
-    # cv2.imshow("adaptive_tresh", cv2.resize(adaptive_tresh, (560, 900)))
-    # cv2.imshow("original", cv2.resize(image, (560, 900)))
-    # cv2.waitKey(0)
-    
-    # create an individual kernals
-    kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (3,80))
+            # read an image with cv2
+            image = cv2.imread(f'./debug/images/page_{i}.jpg')
 
-    # dilate image
-    dilate = cv2.dilate(adaptive_tresh, kernal, iterations=2)
-    # cv2.imwrite(f'./debug/images/page_dilate_{i}.jpg', dilate)
+            # gray the image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # create contours
-    cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    boxes = [cv2.boundingRect(c) for c in cnts]
-    boxes = sorted(boxes, key=lambda x: x[0])
-    merged_boxes = merge_boxes(boxes)
+            # blur an image
+            blur = cv2.GaussianBlur(gray, (7,7), 0)
 
+            # threshold image
+            # thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+            adaptive_tresh_rev = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 51, 5)
+            adaptive_tresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 23, 5)
+            # cv2.imshow("thresh", cv2.resize(thresh, (560, 900)))
+            # cv2.imshow("adaptive_tresh", cv2.resize(adaptive_tresh, (560, 900)))
+            # cv2.imshow("original", cv2.resize(image, (560, 900)))
+            
+            # create an individual kernals
+            kernal = cv2.getStructuringElement(cv2.MORPH_RECT, (3,80))
 
-    for x, y, w, h in merged_boxes:
-        if h > 200 and w > 200:
-            roi = adaptive_tresh[y:y+h, x:x+w]
-            # cv2.rectangle color parameter is BGR
-            cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 1)
-            ocr_result = pytesseract.image_to_string(roi)
-            ocr_result = ocr_result.replace("\n", " ")
-            # print(ocr_result)
-            result.append(ocr_result) if ocr_result not in result else None
+            # dilate image
+            dilate = cv2.dilate(adaptive_tresh_rev, kernal, iterations=1)
+            cv2.imwrite(f'./debug/images/page_dilate_{i}.jpg', dilate)
 
-    cv2.imwrite(f'./debug/images/page_boxes_{i}.jpg', image)
-
-letter_type = None
-insurance = None
+            # create contours
+            cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+            boxes = [cv2.boundingRect(c) for c in cnts]
+            boxes = sorted(boxes, key=lambda x: x[0])
+            merged_boxes = merge_boxes(boxes)
 
 
-# print(" ".join(result))
+            for x, y, w, h in merged_boxes:
+                if h > 200 and w > 200:
+                    roi = adaptive_tresh[y:y+h, x:x+w]
+                    # cv2.rectangle color parameter is BGR
+                    cv2.rectangle(image, (x, y), (x + w, y + h), (36, 255, 12), 1)
+                    ocr_result = pytesseract.image_to_string(roi)
+                    ocr_result = ocr_result.replace("\n", " ")
+                    # print(ocr_result)
+                    result.append(ocr_result) if ocr_result not in result else None
 
-try:
-    for filename in os.listdir(os.path.join(os.getcwd(), "debug\\images")):
-        file_path = os.path.join(os.path.join(os.getcwd(), "debug\\images"), filename)
-        if os.path.isfile(file_path):
-            os.remove(file_path)
-except Exception as e:
-    print(f"Error deleting file - {e}")
-        
-for entity in result:
-    letter_type = determine_letter_type(entity)
-    if letter_type:
-        print("Letter type recognized - ", letter_type)
-        break
+            cv2.imwrite(f'./debug/images/page_boxes_{i}.jpg', image)
 
-for entity in result:
-    insurance = determine_insurance(entity)
-    print("Insurance type recognized - ", insurance)
-    if insurance:
-        break
+        letter_type = None
+        insurance = None
 
-if letter_type and insurance:
-    print(f"Letter type: {letter_type}\nInsurance: {insurance}")
-    match letter_type:
-        case "Approval":
-            print(find_approval_entities(text=" ".join(result), insurance=insurance))
+
+        # print(" ".join(result))
+
+        try:
+            for filename in os.listdir(os.path.join(os.getcwd(), "debug\\images")):
+                file_path = os.path.join(os.path.join(os.getcwd(), "debug\\images"), filename)
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+        except Exception as e:
+            print(f"Error deleting file - {e}")
+                
+        for entity in result:
+            letter_type = determine_letter_type(entity)
+            if letter_type:
+                print("Letter type recognized - ", letter_type)
+                break
+
+        for entity in result:
+            insurance = determine_insurance(entity)
+            if insurance:
+                print("Insurance type recognized - ", insurance)
+                break
+
+        file_result = {
+            "file_name": file,
+            "letter_type": letter_type,
+            "insurance": insurance,
+            "ocr_text": result,
+            "extracted_entities": None
+        }
+
+        if letter_type and insurance:
+            print(f"Letter type: {letter_type}\nInsurance: {insurance}")
+            match letter_type:
+                case "Approval":
+                    file_result["extracted_entities"] = find_approval_entities(text=" ".join(result), insurance=insurance)
+                case "Denial":
+                    file_result["extracted_entities"] =find_denial_entitied(text=" ".join(result), insurance=insurance)
+
+            all_results.append(file_result)
+
+        count += 1
+
+with open("results.json", "w", encoding="utf-8") as f:
+    json.dump(all_results, f, ensure_ascii=False, indent=4)
+
+end_time = time.perf_counter()
+execution_time = end_time - start_time
+print (f"Program ran in {execution_time/60:.2f} minutes\nFinished - {count} letters")
         
 
